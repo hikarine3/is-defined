aa63c53dbc78989437bb5ab6dcabf920b4a6316d
Hajime Kurita <hajimekurita@1stclass.co.jp>
2017-07-24 02:24:24 +0800

    bug fix

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-export default (obj, key) => {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+exports.default = function (obj, key) {
   if (typeof key !== "undefined" && key) {
-    return key.split(".").every(function(x) {
-      if(typeof obj != "object" || obj === null || !x in obj ) {
+    return key.split(".").every(function (x) {
+      if ((typeof obj === "undefined" ? "undefined" : _typeof(obj)) != "object" || obj === null || !x in obj) {
         return false;
       } else {
         obj = obj[x];
         return true;
       }
     });
-  }
-  else{
+  } else {
     if (typeof obj !== "undefined" && obj !== null) {
       return true;
-    }
-    else{
+    } else {
       return false;
     }
   }
-}
+};
